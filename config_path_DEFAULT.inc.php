5c178c0346140b64755a52c16d00fedf1e45a8df
graffio <graffio@lynxlab.com>
2013-08-30 19:32:17 +0200

    default config files

---
+++ config_path_DEFAULT.inc.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Config path
+ *
+ * Defines the roor dir relative config path and requires the main config file
+ *
+ * PHP version >= 5.0
+ *
+ * @author		Stefano Penge <steve@lynxlab.com>
+ * @author		Maurizio "Graffio" Mazzoneschi <graffio@lynxlab.com>
+ * @author		Vito Modena <vito@lynxlab.com>
+ * @copyright	Copyright (c) 2009,  Lynx s.r.l.
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html GNU Public License v.2
+ * @version		0.1
+ */
+  /**
+  *  Root dir relative path
+  */
+  define('ROOT_DIR','/var/www/html/ada');
+/**
+* Main include file
+*/
+require_once(ROOT_DIR.'/config/ada_config.inc.php');
