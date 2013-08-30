d4a46b8897e5f9c97154d9ecdc667e0c30c269cd
graffio <graffio@lynxlab.com>
2013-08-30 19:31:19 +0200

    default config files

---
+++ clients_DEFAULT/client0/client_conf.inc.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Client specific configuration file.
+ *
+ * PHP version >= 5.0
+ *
+ * @author		Stefano Penge <steve@lynxlab.com>
+ * @author		Maurizio "Graffio" Mazzoneschi <graffio@lynxlab.com>
+ * @author		Vito Modena <vito@lynxlab.com>
+ * @copyright   (c) 2009-2010 Lynx s.r.l.
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html GNU Public License v.2
+ */
+
+/**
+ *
+ * @name CLIENT0_DB_TYPE
+ */
+define('CLIENT0_DB_TYPE',  'mysql');
+
+/**
+ *
+ * @name CLIENT0_DB_NAME
+ */
+define('CLIENT0_DB_NAME',  'ada_provider0');
+
+/**
+ *
+ * @name CLIENT0_DB_USER
+ */
+define('CLIENT0_DB_USER',  'user');
+
+/**
+ *
+ * @name CLIENT0_DB_PASS
+ */
+define('CLIENT0_DB_PASS',  'password');
+
+/**
+ *
+ * @name CLIENT0_DB_HOST
+ */
+define('CLIENT0_DB_HOST',  'localhost');
+
+/**
+ *
+ * @name CLIENT0_TIMEZONE
+ */
+define('CLIENT0_TIMEZONE',  'Europe/Rome');
