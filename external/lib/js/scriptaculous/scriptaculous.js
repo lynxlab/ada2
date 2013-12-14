d305255fe5b34e1c27d23332444e2dc443e78a7a
graffio <graffio@lynxlab.com>
2013-12-14 12:22:26 +0100

    added dragdrop in order to make node map work

--- external/lib/js/scriptaculous/scriptaculous.js
+++ external/lib/js/scriptaculous/scriptaculous.js
@@ -49,7 +49,7 @@ var Scriptaculous = {
     }).each( function(s) {
       var path = s.src.replace(/scriptaculous\.js(\?.*)?$/,'');
       var includes = s.src.match(/\?.*load=([a-z,]*)/);
-      (includes ? includes[1] : 'effects').split(',').each(
+      (includes ? includes[1] : 'effects,dragdrop').split(',').each(
 //      (includes ? includes[1] : 'builder,effects,dragdrop,controls,slider,sound').split(',').each(
        function(include) { Scriptaculous.require(path+include+'.js') });
     });
