8f89dfe7f12f1675da0c39b390c7db966fba4402
Giorgio Consorti <g.consorti@lynxlab.com>
2014-03-13 16:51:44 +0100

    completed post method and some fixes

--- api/v1/Controllers/UserController.inc.php
+++ api/v1/Controllers/UserController.inc.php
@@ -21,6 +21,19 @@ require_once ROOT_DIR.'/include/user_classes.inc.php';
 class UserController extends AbstractController implements AdaApiInterface {
 
 	/**
+	 * Users own array key mappings
+	 * 
+	 * @var array
+	 */
+	private static $_userKeyMappings = array (
+			'tipo' => 'type',
+			'codice_fiscale' => 'tax_code',
+			'sesso' => 'gender',
+			'stato' => 'status',
+			'matricola' => 'student_number'
+	);
+
+	/**
 	 * GET method.
 	 * 
 	 * Must be called with id parameter in the params array
@@ -45,7 +58,13 @@ class UserController extends AbstractController implements AdaApiInterface {
 			}
 			
 			if (!is_null($userObj) && !\AMA_DB::isError($userObj)) {
-				return $userObj->toArray();
+				$returnArray =  $userObj->toArray();
+				unset ($returnArray['password']); // hide the password, even if it's encrypted
+				unset ($returnArray['tipo']);     // hide the user type as of 13/mar/2014
+				unset ($returnArray['stato']);    // hide the user status as of 13/mar/2014
+				unset ($returnArray['lingua']);   // hide the user language as of 13/mar/2014
+				self::ADAtoAPIArrayMap($returnArray, self::$_userKeyMappings);
+				return $returnArray;
 			} else {
 				throw new APIException('No User Found', 404);
 			}
@@ -78,44 +97,77 @@ class UserController extends AbstractController implements AdaApiInterface {
 		} else {
 			throw new APIException('Wrong Parameters', 400);
 		}
-		
+
 		/**
-		 * check for missing values: set needed array keys in $neededValues
+		 * Some ADA files are needed for the computation
 		 */
-		$neededValues = array ('nome','cognome','e_mail','birthdate');
-		$missingValues = null;
-				
-		foreach ($neededValues as $value) {
-			if (!isset($userArr[$value]) || strlen($userArr[$value])<=0) {
-				$missingValues[] = $value;
-			}
-		}
-		
-		/**
-		 * If missingValues is still null, it's ok to save
-		 */
-		if (is_null($missingValues))
-		{
-			include_once ROOT_DIR.'/include/token_classes.inc.php';
+		require_once ROOT_DIR.'/include/translator_class.inc.php';
+		require_once ROOT_DIR.'/include/output_funcs.inc.php';
+		require_once ROOT_DIR.'/include/token_classes.inc.php';
+		require_once ROOT_DIR.'/include/Forms/UserRegistrationForm.inc.php';
+		require_once ROOT_DIR.'/comunica/include/MessageHandler.inc.php';
 			
+		/**
+		 * This GLOBAL is needed by the MultiPort and Translator class
+		 */
+		$GLOBALS['common_dh'] = $this->common_dh;
+		
+		/**
+		 * Load supported languages
+		 */
+		\Translator::loadSupportedLanguagesInSession();
+		
+		/**
+		 * Convert API array keys to ADA array keys just
+		 * before instantiating the user object
+		 */
+		self::APItoADAArrayMap($userArr, self::$_userKeyMappings);
+		
+		/**
+		 * Unset the id (if any) to save as new user
+		 */
+		if (isset($userArr['user_id'])) unset ($userArr['user_id']);
+		
+		/**
+		 * Set username to the email.
+		 */
+		$userArr['username'] = $userArr['e_mail'];				
+		$userArr['email'] = $userArr['e_mail'];
+		
+		/**
+		 * Build a user object
+		 */
+		$userObj = new \ADAUser($userArr);
+		$userObj->setLayout('');
+		if (!isset($userArr['type']) || strlen($userArr['type'])<=0) $userObj->setType(AMA_TYPE_STUDENT);
+		
+		/**
+		 * New user is always in a presubscribed status
+		 */
+		$userObj->setStatus(ADA_STATUS_PRESUBSCRIBED);
+		
+		/**
+		 * Generate a random password
+		 */
+		$userObj->setPassword(sha1(time()));
+			
+		/**
+		 * Temporarly set a session user object needed
+		 * to build the UserRegistrationForm and for
+		 * below email message translations
+		 */
+		$_SESSION['sess_userObj'] = $userObj;
+		$form = new \UserRegistrationForm();
+		$form->fillWithArrayData($userArr);	
+
+		/**
+		 * if form is valid, save the user
+		 */
+		if ($form->isValid()) {
 			/**
 			 * The user is associated by default to the public tester.
 			 */
 			$regProvider = array (ADA_PUBLIC_TESTER);
-			
-			/**
-			 * Proceed to the user registration.
-			 */
-			$userArr['username'] = $userArr['e_mail'];
-			// prevents a bug in include/user_class.inc.php line 377
-			$userArr['email'] = $userArr['e_mail'];
-			
-			$userObj = new \ADAUser($userArr);
-			$userObj->setLayout('');
-			$userObj->setType(AMA_TYPE_STUDENT);
-			$userObj->setStatus(ADA_STATUS_PRESUBSCRIBED);
-			// Random password.
-			$userObj->setPassword(sha1(time()));
 			
 			/**
 			 * Save the user in the public tester and in
@@ -128,27 +180,124 @@ class UserController extends AbstractController implements AdaApiInterface {
 				}
 			}
 			
-			// This GLOBAL is needed by the MultiPort
-			$GLOBALS['common_dh'] = $this->common_dh;
 			$id_user = \Multiport::addUser($userObj,$regProvider);
-			
-			if ($id_user < 0) {				
+				
+			if ($id_user < 0) {
 				// an error occoured
-				$saveResults = array(
-					'status'=>'FAILURE',
-					'message'=>'Check if a user exists already having passed email and username'
-				);
+				$saveResults = array( 'status'=>'FAILURE',
+									  'message'=>'Check if a user exists already having passed email and username');
 			} else {
 				// saved ok
-				$saveResults = array(
-					'status'=>'SUCCESS',
-					'user_id'=>$id_user
-				);
+				$saveResults = array( 'status'=>'SUCCESS',
+									  'user_id'=>$id_user);
+				/**
+				 * Set HTTP status to 201: Created before returning
+				 */
+				$this->slimApp->status(201);
+				
+				/**
+				 * Build and send a registration email as per browsing/registration.php file
+				 */
+				
+				/**
+				 * Create a registration token for this user and send it to the user
+				 * with the confirmation request.
+				 */
+				$tokenObj = \TokenManager::createTokenForUserRegistration($userObj);
+				if($tokenObj != false) {
+					
+					$token = $tokenObj->getTokenString();
+					
+					$admtypeAr = array(AMA_TYPE_ADMIN);
+					$admList = $this->common_dh-> get_users_by_type($admtypeAr);
+					if (!\AMA_DataHandler::isError($admList)){
+						$adm_uname = $admList[0]['username'];
+					} else {
+						$adm_uname = ADA_ADMIN_MAIL_ADDRESS;
+					}
+					
+					$title = PORTAL_NAME.': ' . translateFN('ti chiediamo di confermare la registrazione.');
+					
+					
+					$text = sprintf(translateFN('Gentile %s, ti chiediamo di confermare la registrazione ai %s.'),
+							$userObj->getFullName(), PORTAL_NAME)
+							. PHP_EOL . PHP_EOL
+							. translateFN('Il tuo nome utente è il seguente:')
+							. ' ' . $userObj->getUserName()
+							. PHP_EOL . PHP_EOL
+							. sprintf(translateFN('Puoi confermare la tua registrazione ai %s seguendo questo link:'),
+									PORTAL_NAME)
+									. PHP_EOL
+									. ' ' . HTTP_ROOT_DIR."/browsing/confirm.php?uid=$id_user&tok=$token"
+									. PHP_EOL . PHP_EOL;
+					
+					$message_ha = array(
+							'titolo' => $title,
+							'testo' => $text,
+							'destinatari' => array($userObj->getUserName()),
+							'data_ora' => 'now',
+							'tipo' => ADA_MSG_SIMPLE,
+							'mittente' => $adm_uname
+					);
+					
+					$mh = \MessageHandler::instance(\MultiPort::getDSN($tester));
+					
+					/**
+					 * Send the message as an internal message,
+					 * don't care if an error occours here
+					*/
+					$result = $mh->send_message($message_ha);
+					
+					/**
+					 * Send the message as an email message
+					 */
+					$message_ha['tipo'] = ADA_MSG_MAIL;
+					$result = $mh->send_message($message_ha);
+					if(\AMA_DataHandler::isError($result)) {
+						$saveResults['message'] = 'An error occoured while emailing the user.';
+					}
+				} else {
+					$saveResults['message'] = 'An error occourred while building the confirmation token.';
+				}
+				
+				/**
+				 * Done email sending.
+				 */
+				if (isset($saveResults['message']) && strlen($saveResults['message'])>0) {
+					$saveResults['message'] .= 'The confirmation email has not been sent, please contact the user directly.';
+				}				
 			}
 			return $saveResults;
 		} else {
-			throw new APIException('Missing User Values: '.implode(',', $missingValues), 400);
+			/**
+			 * Try to investigate what the missing fields are
+			 */
+			foreach ($form->getControls() as $control) {
+				if ($control->getIsMissing()) {
+					/**
+					 * Build an array with missing fields as keys
+					 */
+					$missingValues[$control->getId()] = TRUE;
+				}
+			}
+			if (isset($missingValues) && sizeof($missingValues)>0) {
+				/**
+				 * Map the missingValues keys to API keys
+				 */
+				self::ADAtoAPIArrayMap($missingValues,self::$_userKeyMappings);
+				/**
+				 * Extract the missingValues keys to build the
+				 * list of missing or invalid value
+				 */
+				$missingValues = implode(', ', array_keys($missingValues));
+			} else {
+				$missingValues = 'Unable to build missing fields list';
+			}
+			throw new APIException('Missing or Invalid User Fields: '.$missingValues, 400);
 		}
+		
+		unset ($_SESSION['sess_userObj']);
+		
 	}
 	
 	public function put    (array $params = array()) {}
