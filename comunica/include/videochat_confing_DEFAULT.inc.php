c2984dbb67d530fa9742cfc9bf602b02583b71aa
sara <sara@lynxlab.com>
2014-04-23 17:38:57 +0200

    Added gitgnore e videocaht_config_DEFAULT file

---
+++ comunica/include/videochat_confing_DEFAULT.inc.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Openmeetings specific configuration file.
+ * 
+ * @author
+ * @version
+ * @package
+ * @license
+ * @copyright (c) 2009 Lynx s.r.l.
+ */
+
+/**
+ * 
+ * @name OPENMEETINGS DATA SERVER 
+ */
+
+define('OPENMEETINGS_HOST',  'URL');
+define('OPENMEETINGS_PORT',  ':5080');
+define('OPENMEETINGS_ADMIN',  'user');
+define('OPENMEETINGS_PASSWD',  '');
+define('OPENMEETINGS_DIR',  'openmeetings');
+
+
+
+/**
+ * 
+ * @name OPENMEETINGS DEFAULT DATA ROOM 
+ */
+
+define('ROOM_DEFAULT_LANGUAGE',  '4');
+define('ROOM_IS_PUBLIC',  'true');
+define('VIDEO_POD_WIDTH',  '355');
+define('VIDEO_POD_HEIGHT',  '560');
+define('VIDEO_POD_X_POSITION',  '2');
+define('VIDEO_POD_y_POSITION',  '2');
+define('MODERATION_PANEL_X_POSITION',  '400');
+define('SHOW_WHITE_BOARD',  'true');
+define('WHITE_BOARD_PANEL_X_POSITION',  '360');
+define('WHITE_BOARD_PANEL_Y_POSITION',  '2');
+define('WHITE_BOARD_PANEL_HEIGHT',  '560');
+define('WHITE_BOARD_PANEL_WIDTH',  '600');
+define('SHOW_FILES_PANEL',  'false');
+define('FILES_PANEL_X_POSITION',  '2');
+define('FILES_PANEL_Y_POSITION',  '284');
+define('FILES_PANEL_HEIGHT',  '310');
+define('FILES_PANEL_WIDTH',  '270');
+
+// ***********
+define('CONFERENCE_TYPE','1');
+define('AUDIENCE_TYPE','3'); 
+
+//*******
+define('FRAME_WIDTH','1000');
+define('FRAME_HEIGHT','600');
+
+define('VIDEOCHAT_LANGUAGE_BG', '30');
+define('VIDEOCHAT_LANGUAGE_EN', '1');
+define('VIDEOCHAT_LANGUAGE_ES', '8');
+//define('VIDEOCHAT_LANGUAGE_ES', '6'); // versione 0
+define('VIDEOCHAT_LANGUAGE_IS', '1');
+define('VIDEOCHAT_LANGUAGE_IT', '5');
+define('VIDEOCHAT_LANGUAGE_RO', '1');
+define('VIDEOCHAT_LANGUAGE_FR', '4');
+define('VIDEOCHAT_LANGUAGE_DE', '2');
+
+define('DATE_CONTROL',FALSE);
+
+define('OPENMEETINGS_VERSION','1');
+
+?>
+ 
