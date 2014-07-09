04c402dc9fcc7ef6a0bdeabb409a9387000da123
sara <sara@lynxlab.com>
2014-07-09 17:17:07 +0200

    fixed bug in styles

--- tutor/edit_tutor.php
+++ tutor/edit_tutor.php
@@ -110,7 +110,7 @@ $content_dataAr = array(
     'messages' => $user_messages->getHtml(),
     'agenda' => $user_agenda->getHtml(),
     'status' => $status,
-    'course_title' => translateFN('Modifica dati utente'),
+    'path' => translateFN('Modifica dati utente'),
     'data' => $form->getHtml(),
     'help' => $help
 );
