4fe6f30a14106be99b12fc55abae6e760b3be417
Giorgio Consorti <g.consorti@lynxlab.com>
2014-09-02 18:16:07 +0200

    table changes for revised item-enabledON feature

--- db/ada_menu.sql
+++ db/ada_menu.sql
@@ -56,3 +56,14 @@ CREATE TABLE IF NOT EXISTS `menu_tree` (
   `extraClass` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`tree_id`,`parent_id`,`item_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- `menu_items` table changes for revised enabled feature
+--
+ALTER TABLE `menu_items` ADD `enabledON` VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '%ALWAYS%' ;
+UPDATE `menu_items` SET `enabledON` = '%ALWAYS%' WHERE enabled=1;
+UPDATE `menu_items` SET `enabledON` = '%NEVER%'  WHERE enabled=0;
+ALTER TABLE `menu_items` DROP `enabled`;
+
