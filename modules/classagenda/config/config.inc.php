6dc0b1c2387b01cd231c4f9bffb0919b931a7189
Giorgio Consorti <g.consorti@lynxlab.com>
2015-01-20 18:11:36 +0100

    added module's own defines to filter course instances

--- modules/classagenda/config/config.inc.php
+++ modules/classagenda/config/config.inc.php
@@ -12,8 +12,12 @@
 
 require_once MODULES_CLASSAGENDA_PATH.'/include/AMAClassagendaDataHandler.inc.php';
 
-define('MODULES_CLASSAGENDA_EDIT_CAL',			1); // edit calendar action code
-define('MODULES_CLASSAGENDA_DO_ROLLCALL',		2); // do the class roll call action code
-define('MODULES_CLASSAGENDA_DO_ROLLCALLHISTORY',3); // roll call history action code
+define('MODULES_CLASSAGENDA_EDIT_CAL',				1); // edit calendar action code
+define('MODULES_CLASSAGENDA_DO_ROLLCALL',			2); // do the class roll call action code
+define('MODULES_CLASSAGENDA_DO_ROLLCALLHISTORY',	3); // roll call history action code
+
+define('MODULES_CLASSAGENDA_ALL_INSTANCES',			1); // filter all course instances
+define('MODULES_CLASSAGENDA_STARTED_INSTANCES',		2); // filter started course instances
+define('MODULES_CLASSAGENDA_NONSTARTED_INSTANCES',	3); // filter non started course instances
 
 ?>
