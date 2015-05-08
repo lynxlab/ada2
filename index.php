487ab7b5aa9749627c7de9151c901e32d0eb299d
Giorgio Consorti <g.consorti@lynxlab.com>
2015-05-08 15:50:31 +0200

    added AMA_TYPE_SWITCHER to the $allowedUsersAr

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ require_once realpath(dirname(__FILE__)).'/config_path.inc.php';
  * Clear node and layout variable in $_SESSION
  * $_SESSION was destroyed, so we do not need to clear data in session.
  */
-$allowedUsersAr = array(AMA_TYPE_VISITOR, AMA_TYPE_STUDENT,AMA_TYPE_TUTOR, AMA_TYPE_AUTHOR, AMA_TYPE_ADMIN);
+$allowedUsersAr = array(AMA_TYPE_VISITOR, AMA_TYPE_STUDENT,AMA_TYPE_TUTOR, AMA_TYPE_AUTHOR, AMA_TYPE_ADMIN, AMA_TYPE_SWITCHER);
 /**
  * Performs basic controls before entering this module
  */
