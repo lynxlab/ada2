3c0861a9102d214e2e8d1b914ecbca99ef58e87a
Giorgio Consorti <g.consorti@lynxlab.com>
2015-06-18 16:36:34 +0200

    several LDAP fixes, imporvements and mapping of LDAP user group to AMA_TYPE _* role

--- modules/login/include/ldapLogin.class.inc.php
+++ modules/login/include/ldapLogin.class.inc.php
@@ -11,75 +11,143 @@
 
 /**
  * LDAP login provider implementation
- * 
- * this is working from the command line
- * 
- * ldapsearch -x -b "ou=users,dc=lynxlab,dc=com" \
-*  -W -H ldap://lynxlab.com -D "uid=YOUR_USER_ID_HERE,ou=users,dc=lynxlab,dc=com"
- * 
  */
 class ldapLogin extends AbstractLogin
 {
 	/**
-	 * performs user login using the ADA db
+	 * performs user login using an LDAP server
 	 * 
 	 * (non-PHPdoc)
 	 * @see iLogin::doLogin()
 	 */
 	public function doLogin($name, $pass, $remindMe, $language)
 	{
-		
-		$options = $this->loadOptions();
-		
-		$ldap_dn = $options['dn'];
-		$ldap_uri = $options['uri'];
-		
-		$handle = ldap_connect($ldap_uri);
-		// this will output a warning in the webserver log on failure
-		$bind = ldap_bind($handle, 'uid='.$name.','.$ldap_dn, $pass);
-		$result = ldap_search($handle, $ldap_dn, "uid=".$name);
-		
-		if ($bind !==false && $result!==false) {
+		try {
+			$options = $this->loadOptions();
 			/**
-			 * look if user is already in ADA DB
+			 * check LDAP configuration in module's option table
 			 */
-			$userObj = $this->checkADAUser($name);
-			
-			if (!is_object($userObj) || !$userObj instanceof ADALoggableUser) {
+			if (!is_null($options) && is_array($options) && count($options)>0) {
+				// mandatory fields
+				$mandatoryOptions = array(
+						'host' => 'Impostare l\'host LDAP',
+						'dn'  => 'Impostare il dn LDAP',
+						'ou_users' => 'Impostare l\'ou per gli utenti in LDAP',
+						'ou_groups'=> 'Impostare l\'ou per i gruppi in LDAP'
+				);
 				
-				$entries = ldap_get_entries($handle, $result);
-				ldap_unbind($handle);
-				if ($entries!==false && is_array($entries) && count($entries)>0) {
-					$entries = $entries[0];
-					
-					/**
-					 * build user array
-					 */
-					$adaUser = array(
-							'nome' => $entries['givenname'][0],
-							'cognome' => $entries['sn'][0],
-							'email' => 'nobody',
-							'username' => $entries['uid'][0],
-							'cap' => '',
-							'matricola' => '',
-							'avatar' => '',
-							'birthcity' => ''						
-					);
-					return $this->addADAUser($adaUser);
-				}				
-			} // user not found in ADA
+				foreach ($mandatoryOptions as $optionName=>$errorMessage) {
+					if (!array_key_exists($optionName, $options) || strlen($options[$optionName])<=0) {
+						$errorMessage = translateFN($errorMessage) .
+										'<br/>(key=\''.$optionName.'\' '.translateFN('nelle opzioni').')';
+						throw new Exception($errorMessage);
+					}
+				}
+			} else throw new Exception(translateFN('Impossibile caricare la configurazione LDAP'));
+			// connect to host
+			$handle = ldap_connect($options['host']);
+			// set options
+			ldap_set_option($handle, LDAP_OPT_PROTOCOL_VERSION, 3 );
+			ldap_set_option($handle, LDAP_OPT_REFERRALS, 0);
+			ldap_set_option($handle, LDAP_OPT_NETWORK_TIMEOUT,  30); /* 30 second timeout */
 			
-			/**
-			 * At this point, either the $userObj was already in
-			 * ADA DB or had just been created by the above code
-			 */
-			if (is_object($userObj) && $userObj instanceof ADALoggableUser) {
-				return $userObj;
+			// this will output a warning in the webserver log on failure
+			$bind = ldap_bind($handle, 'uid='.$name.',ou='.$options['ou_users'].','.$options['dn'], $pass);
+			
+			if ($bind !==false) {
+				/**
+				 * look if user is already in ADA DB
+				 */
+				$userObj = $this->checkADAUser($name);
+				
+				if (!is_object($userObj) || !$userObj instanceof ADALoggableUser) {
+					/**
+					 * If user is not in ADA DB, try loading his data from LDAP
+					 */
+					$result = ldap_search($handle, $options['dn'], "uid=".$name);
+					/**
+					 * If $results is false, throw an exception
+					 */
+					if ($result!==false) $entries = ldap_get_entries($handle, $result);
+					else throw new Exception(ldap_err2str(ldap_errno($handle)), ldap_errno($handle));
+					
+					if ($entries!==false && is_array($entries) && count($entries)>0) {
+						$entries = $entries[0];
+						/**
+						 * Look for user group name from group id and map it to proper AMA_TYPE
+						 * accordingly to module's option table
+						 */
+						if (!is_null($entries) && isset($entries['gidnumber']) && intval($entries['gidnumber'][0])>0) {
+							// look for group name
+							$query = "(&(objectClass=posixGroup)(gidNumber=".$entries['gidnumber'][0]."))";
+							$groupres = ldap_search($handle, "ou=".$options['ou_groups'].",".$options['dn'], $query);
+							/**
+							 * If $groupres is false, don't thrown an exception: the user shall be a student
+							 */
+							$groupentries = ldap_get_entries($handle, $groupres);
+							
+							if (isset($groupentries[0]['cn']) && strlen($groupentries[0]['cn'][0])>0) {
+								$groupname = $groupentries[0]['cn'][0];
+								$possibleUserType = null;
+								/**
+								 * this will map LDAP groups to AMA_TYPE_* 
+								 * accordingly to the options table
+								 */
+								foreach ($options as $optionName=>$groupDesc) {
+									if (strpos($optionName,'AMA_TYPE')===0) {
+										if (is_array($groupDesc) && in_array($groupname, $groupDesc)) {
+											// if value (aka $groupDesc) in the options table is array
+											$possibleUserType = strtoupper($optionName);
+											break;
+										} else if (strcmp($groupDesc, $groupname)===0) {
+											// if value (aka $groupDesc) in the options table is string
+											$possibleUserType = strtoupper($optionName);
+											break;
+										}
+									}
+								}
+								
+								if (!is_null($possibleUserType) && defined($possibleUserType)) $userType = constant($possibleUserType);
+							}
+						}
+						
+						/**
+						 * build user array
+						 */
+						$adaUser = array(
+								'nome' => $entries['givenname'][0],
+								'cognome' => $entries['sn'][0],
+								'email' => 'nobody',
+								'username' => $entries['uid'][0],
+								'tipo' => isset($userType) ? $userType  : AMA_TYPE_STUDENT,
+								'cap' => '',
+								'matricola' => '',
+								'avatar' => '',
+								'birthcity' => ''
+						);
+						
+						if (isset($handle) && !is_null($handle)) ldap_unbind($handle);
+						return $this->addADAUser($adaUser);
+					}
+				} // user not found in ADA
+				
+				/**
+				 * At this point, either the $userObj was already in
+				 * ADA DB or had just been created by the above code
+				 */
+				if (is_object($userObj) && $userObj instanceof ADALoggableUser) {
+					return $userObj;
+				}
+			} else {
+				throw new Exception(ldap_err2str(ldap_errno($handle)), ldap_errno($handle));
 			}
-		} // user not found in LDAP
-		else {
+		} catch (Exception $e) {
 			ldap_unbind($handle);
-			return new Exception(translateFN("Username  e/o password non valide"));
+			// 'Invalid credentials' (code:49)  gets ADA's own message as text
+			if ($e->getCode()==49) {
+				return new Exception(translateFN("Username  e/o password non valide"));
+			}
+			return $e;
 		}
 	}
 }
