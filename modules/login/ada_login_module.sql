28a6717be3821aa294e8051afdb5471d9316dbb6
Giorgio Consorti <g.consorti@lynxlab.com>
2015-06-19 09:50:04 +0200

    added sql file for module tables creation

---
+++ modules/login/ada_login_module.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.3deb1.trusty~ppa.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Giu 19, 2015 alle 09:45
+-- Versione del server: 5.5.43-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `module_login_history_login`
+--
+
+CREATE TABLE IF NOT EXISTS `module_login_history_login` (
+  `id_utente` int(10) unsigned NOT NULL,
+  `date` int(11) NOT NULL,
+  `module_login_providers_id` int(5) unsigned NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `module_login_options`
+--
+
+CREATE TABLE IF NOT EXISTS `module_login_options` (
+  `module_login_providers_id` int(5) unsigned NOT NULL,
+  `key` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `value` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `module_login_providers`
+--
+
+CREATE TABLE IF NOT EXISTS `module_login_providers` (
+`module_login_providers_id` int(5) unsigned NOT NULL,
+  `className` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
+  `buttonLabel` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `displayOrder` int(4) unsigned NOT NULL
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `module_login_history_login`
+--
+ALTER TABLE `module_login_history_login`
+ ADD PRIMARY KEY (`id_utente`,`date`,`module_login_providers_id`);
+
+--
+-- Indexes for table `module_login_options`
+--
+ALTER TABLE `module_login_options`
+ ADD PRIMARY KEY (`module_login_providers_id`,`key`);
+
+--
+-- Indexes for table `module_login_providers`
+--
+ALTER TABLE `module_login_providers`
+ ADD PRIMARY KEY (`module_login_providers_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `module_login_providers`
+--
+ALTER TABLE `module_login_providers`
+MODIFY `module_login_providers_id` int(5) unsigned NOT NULL AUTO_INCREMENT;
