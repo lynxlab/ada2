f635a7bd6d176a945515b1067f13a91ec5dd8bab
Giorgio Consorti <g.consorti@lynxlab.com>
2016-02-02 10:06:39 +0100

    add support for slideimport module

--- js/services/author.js
+++ js/services/author.js
@@ -1,9 +1,9 @@
 function initDoc(){
-    
+
     var lastCol = $j('table.doDataTable thead th').length;
     var colDefs = null;
     var moreColDefs = null;
-    
+
     colDefs = [{
     	"aTargets": [lastCol-1],
     	"sWidth" : "1%",
@@ -21,6 +21,10 @@ function initDoc(){
     if ($j('#authorTable').length>0) {
     	moreColDefs = [{"aTargets": [lastCol-3], "sWidth" : "1%", "bSortable":false, "sClass" : "actionCol" },
     	               {"aTargets": [2], "sType":"date-eu" }];
+    	// this column is an extra column only needed if there's a module with an action
+    	if (lastCol-4 > 0) {
+    		moreColDefs.push({"aTargets": [lastCol-4], "sWidth" : "1%", "bSortable":false, "sClass" : "actionCol" });
+    	}
     } else if ($j('#authorReport').length>0) {
     	moreColDefs = [{"aTargets": [lastCol-3], "sWidth" : "7%", "sClass":"centerAlign" },
     	               {"aTargets": [0], "sType":"formatted-num" }];
@@ -28,10 +32,10 @@ function initDoc(){
     	colDefs = [{"aTargets": [lastCol-1], "sWidth" : "15%", "sClass" : "centerAlign" },
     	           {"aTargets": [0], "sType":"date-eu" }];
     }
-    
+
     if (colDefs == null) colDefs=[];
     if (moreColDefs != null) for (var x=0; x<moreColDefs.length; x++) colDefs.push(moreColDefs[x]);
-    
+
     datatable = $j('table.doDataTable').dataTable({
 		"bJQueryUI": true,
         "bFilter": true,
