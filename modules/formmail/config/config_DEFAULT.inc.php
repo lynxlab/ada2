d3a81f705be6a957885e460b5a7a92376699cdcc
Giorgio Consorti <g.consorti@lynxlab.com>
2016-03-25 17:32:29 +0100

    add callback function to enable proper menu item
    formmail menu item will be enabled for switcher users only

--- modules/formmail/config/config_DEFAULT.inc.php
+++ modules/formmail/config/config_DEFAULT.inc.php
@@ -10,4 +10,22 @@
  * @version		   0.1
  */
 
+/**
+ * callback function used to check if formmail menu item is to be enabled
+ *
+ * @param array $allowedTypes optional, can be defined in the DB enabledON field or in the function body
+ *
+ * @return true if menu must be enabled
+ */
+function menuEnableFormMail($allowedTypes=null) {
+
+	if (is_null($allowedTypes)) {
+		/**
+		 * Add here user types for which the formmail menu must be enabled
+		 */
+		$allowedTypes = array(AMA_TYPE_SWITCHER);
+	}
+
+	return defined('MODULES_FORMMAIL') && MODULES_FORMMAIL && isset($_SESSION['sess_userObj']) && in_array($_SESSION['sess_userObj']->getType(), $allowedTypes);
+}
 ?>
