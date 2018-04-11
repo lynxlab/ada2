7419baccc147288b39500c4a98ce26687fcfe53f
Giorgio Consorti <g.consorti@lynxlab.com>
2018-04-11 13:37:33 +0200

    add code to handle close request button click

--- modules/gdpr/js/list.js
+++ modules/gdpr/js/list.js
@@ -5,9 +5,11 @@
  * @license		http://www.gnu.org/licenses/gpl-2.0.html GNU Public License v.2
  * @version		0.1
  */
+var clickHandlers = {};
 
 function initDoc(tableID, options) {
 
+	var debug = false;
 	$j.fn.dataTable.ext.errMode = 'throw';
 	var showall = ('showall' in options) ? options.showall : false;
 	var columns = [
@@ -28,15 +30,15 @@ function initDoc(tableID, options) {
 				}
 			}
 		},
-		{ "data": "generateDate",
+		{ "data": "generatedDate",
 			"className": "dt-body-center",
 			"type": "date-euro",
-			"width" : "15%"
+			"width" : "10%"
 		},
 		{ "data": "closedDate",
 			"className": "dt-body-center",
 			"type": "date-euro",
-			"width" : "15%",
+			"width" : "10%",
 		    "render": function ( data, type, row, meta ) {
 		    	return (null === data) ? '' : data ;
 		    }
@@ -55,7 +57,7 @@ function initDoc(tableID, options) {
 	
 	var requestTypeClassNames = [ '', 'access', 'edit', 'onhold', 'delete' ];
 	
-	$j('#'+tableID)
+	var tableObj = $j('#'+tableID)
 		.DataTable({
 			"deferRender": true,
 			"processing" : true,
@@ -73,4 +75,60 @@ function initDoc(tableID, options) {
 			},
 			"columns": columns
 		});
+
+	clickHandlers.closeRequest = function (buttonObj, requestUUID) {
+		if (!buttonObj.hasClass('disabled')) {			
+		    var url = "ajax/forceCloseRequest.php";
+		    var showHidePromise;
+		    var reloadCallback = null;
+		    var clickedIndex = tableObj.cell(buttonObj.parents('td').first()).index();
+		    return $j.ajax({
+		    	type: "POST",
+		    	url: url,
+		    	data: { requestUUID: requestUUID, debug: debug ? 1 :0 },
+		    	beforeSend: function() {
+		    		buttonObj.addClass('disabled');
+		    	}
+		    })
+		    .done(function(response) {
+		    	if (debug) console.log('done callback got ', response);
+		    	reloadCallback = function() {
+		    		tableObj.row(clickedIndex.row).nodes().to$().addClass('yellow-highlight');
+		    		setTimeout(function() {
+		    			tableObj.row(clickedIndex.row).nodes().to$().removeClass('yellow-highlight');
+		    		},5000);
+		    	};
+		    })
+		    .fail(function(response) {
+		    	if (debug) console.log('fail callback ', response);
+		    	if ('responseJSON' in response) {
+
+		    		if (debug) {
+						console.groupCollapsed(url+' fail');
+						if ('errorMessage' in response.responseJSON) {
+							console.error('message: %s', response.responseJSON.errorMessage);
+						}
+						if ('errorTrace' in response.responseJSON) {
+							console.error('stack trace %s', response.responseJSON.errorTrace);
+						}
+						console.groupEnd();
+		    		}
+
+		    		showHidePromise = showHideDiv(response.responseJSON.title, response.responseJSON.message, false);
+
+		    	} else {
+		    		var errorText = response.statusText;
+		    		if ('responseText' in response && response.responseText.length>0) errorText += '<br/>'+response.responseText;
+		    		showHidePromise = showHideDiv('Error ' + response.status, errorText, false);
+		    	}	
+		    })
+		    .always(function(response) {
+		    	if (debug) console.log('always callback');
+		    	$j.when(showHidePromise).then(function(){
+		    		// reload data, will enable the disabled button
+		    		tableObj.ajax.reload(reloadCallback, false);
+		    	});
+		    });
+		}
+	};
 }
