b80ff22568a0b7a881a2d2609d428c7cd0fe0aa5
Giorgio Consorti <g.consorti@lynxlab.com>
2018-04-18 11:44:11 +0200

    fix handleRequest function to show a confirm action modal when needed

--- modules/gdpr/js/list.js
+++ modules/gdpr/js/list.js
@@ -88,60 +88,73 @@ function handleRequest(tableObj, buttonObj, objData) {
 	    	showHidePromise, 
 	    	reloadCallback = null,
 	    	clickedIndex = tableObj.cell(buttonObj.parents('td').first()).index(),
-	    	reloadData = true;
-	    return $j.ajax({
-	    	type: "POST",
-	    	url: url,
-	    	data: objData,
-	    	beforeSend: function() {
-	    		buttonObj.addClass('disabled');
-	    	}
-	    })
-	    .done(function(response) {
-	    	if (debug) console.log('done callback got ', response);
-	    	reloadData = 'reloaddata' in response && response.reloaddata;
-	    	if ('redirecturl' in response && response.redirecturl.trim().length>0) {
-	    		// if response has a redirect, obey at once!
-	    		document.location.href = response.redirecturl.trim();
-	    	}
-	    	reloadCallback = function() {
-	    		tableObj.row(clickedIndex.row).nodes().to$().addClass('yellow-highlight');
-	    		setTimeout(function() {
-	    			tableObj.row(clickedIndex.row).nodes().to$().removeClass('yellow-highlight');
-	    		},5000);
-	    	};
-	    })
-	    .fail(function(response) {
-	    	if (debug) console.log('fail callback ', response);
-	    	if ('responseJSON' in response) {
+	    	reloadData = true,
+	    	confirmhandle = objData.confirmhandle || false,
+	    	confirmModalID = '#confirmModal',
+	    	requesttype = objData.requesttype || false;
 
-	    		if (debug) {
-					console.groupCollapsed(url+' fail');
-					if ('errorMessage' in response.responseJSON) {
-						console.error('message: %s', response.responseJSON.errorMessage);
-					}
-					if ('errorTrace' in response.responseJSON) {
-						console.error('stack trace %s', response.responseJSON.errorTrace);
-					}
-					console.groupEnd();
-	    		}
+		var ajaxHandleRequest = function() {
+		    return $j.ajax({
+		    	type: "POST",
+		    	url: url,
+		    	data: objData,
+		    	beforeSend: function() {
+		    		buttonObj.addClass('disabled');
+		    	}
+		    })
+		    .done(function(response) {
+		    	if (debug) console.log('done callback got ', response);
+		    	reloadData = 'reloaddata' in response && response.reloaddata;
+		    	if ('redirecturl' in response && response.redirecturl.trim().length>0) {
+		    		// if response has a redirect, obey at once!
+		    		document.location.href = response.redirecturl.trim();
+		    	}
+		    	reloadCallback = function() {
+		    		tableObj.row(clickedIndex.row).nodes().to$().addClass('yellow-highlight');
+		    		setTimeout(function() {
+		    			tableObj.row(clickedIndex.row).nodes().to$().removeClass('yellow-highlight');
+		    		},5000);
+		    	};
+		    })
+		    .fail(function(response) {
+		    	if (debug) console.log('fail callback ', response);
+		    	if ('responseJSON' in response) {
 
-	    		showHidePromise = showHideDiv(response.responseJSON.title, response.responseJSON.message, false);
+		    		if (debug) {
+						console.groupCollapsed(url+' fail');
+						if ('errorMessage' in response.responseJSON) {
+							console.error('message: %s', response.responseJSON.errorMessage);
+						}
+						if ('errorTrace' in response.responseJSON) {
+							console.error('stack trace %s', response.responseJSON.errorTrace);
+						}
+						console.groupEnd();
+		    		}
 
-	    	} else {
-	    		var errorText = response.statusText;
-	    		if ('responseText' in response && response.responseText.length>0) errorText += '<br/>'+response.responseText;
-	    		showHidePromise = showHideDiv('Error ' + response.status, errorText, false);
-	    	}	
-	    })
-	    .always(function(response) {
-	    	if (debug) console.log('always callback');
-	    	if (reloadData) {
-	    		$j.when(showHidePromise).then(function(){
-	    			// reload data, will enable the disabled button
-	    			tableObj.ajax.reload(reloadCallback, false);
-	    		});
-	    	}
-	    });
+		    		showHidePromise = showHideDiv(response.responseJSON.title, response.responseJSON.message, false);
+
+		    	} else {
+		    		var errorText = response.statusText;
+		    		if ('responseText' in response && response.responseText.length>0) errorText += '<br/>'+response.responseText;
+		    		showHidePromise = showHideDiv('Error ' + response.status, errorText, false);
+		    	}
+		    })
+		    .always(function(response) {
+		    	if (debug) console.log('always callback');
+		    	if (reloadData) {
+		    		$j.when(showHidePromise).then(function(){
+		    			// reload data, will enable the disabled button
+		    			tableObj.ajax.reload(reloadCallback, false);
+		    		});
+		    	}
+		    });
+		};
+
+		if (confirmhandle) {
+			$j('.confirmText', confirmModalID).hide();
+			$j('.confirmText[data-requesttype='+requesttype+']', confirmModalID).show();
+			semanticConfirm(confirmModalID, { onApprove: ajaxHandleRequest } );
+		} else ajaxHandleRequest();
+
 	}
 }
