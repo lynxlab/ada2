80196a583916da33a25bfed0a10a76808165e5df
Giorgio Consorti <g.consorti@lynxlab.com>
2018-05-03 17:21:24 +0200

    fix to display an error message if module autoloader cannot be found

--- modules/gdpr/config/config.inc.php
+++ modules/gdpr/config/config.inc.php
@@ -7,4 +7,26 @@
  * @version		0.1
  */
 
-require_once dirname(__FILE__).'/../vendor/autoload.php';
+try {
+	if (! @include_once(dirname(__FILE__).'/../vendor/autoload.php')) {
+		// @ - to suppress warnings,
+		throw new Exception (
+			json_encode(array(
+				'header' => 'GDPR module will not work because autoload file cannot be found!',
+				'message' => 'Please run <code>composer install</code> in the module subdir'
+		)));
+	} else return true;
+} catch(Exception $e) {
+	$text = json_decode($e->getMessage(), true);
+	// populating $_GET['message'] is a dirty hack to force the error message to appear in the home page at least
+	if (!isset($_GET['message'])) $_GET['message'] = '';
+	$_GET['message'] .= '<div class="ui icon error message"><i class="ban circle icon"></i><div class="content">';
+	if (array_key_exists('header', $text) && strlen($text['header'])>0) {
+		$_GET['message'] .= '<div class="header">'.$text['header'].'</div>';
+	}
+	if (array_key_exists('message', $text) && strlen($text['message'])>0) {
+		$_GET['message'] .= '<p>'.$text['message'].'</p>';
+	}
+	$_GET['message'] .= '</div></div>';
+}
+return false;
