b85c5efc671f30cd855ddf5a69cd8591e07d96a4
Giorgio Consorti <g.consorti@lynxlab.com>
2018-06-22 16:40:56 +0200

    fix minor bugs issuing php notices and warnings

--- modules/service-complete/include/completeConditionSet.class.inc.php
+++ modules/service-complete/include/completeConditionSet.class.inc.php
@@ -3,7 +3,7 @@
  * SERVICE-COMPLETE MODULE.
  *
  * @package        service-complete module
- * @author         Giorgio Consorti <g.consorti@lynxlab.com>         
+ * @author         Giorgio Consorti <g.consorti@lynxlab.com>
  * @copyright      Copyright (c) 2013, Lynx s.r.l.
  * @license        http://www.gnu.org/licenses/gpl-2.0.html GNU Public License v.2
  * @link           service-complete
@@ -13,7 +13,7 @@
 /**
  * class to represent the set of condition as defined
  * by the user.
- * 
+ *
  * this is a sequence of operation represented by
  * the operations property that is an operation object
  * representing the tree of operations to be done
@@ -24,69 +24,69 @@ class CompleteConditionSet
 {
 	/**
 	 * The id of the condition set, as stored in the DB
-	 * 
+	 *
 	 * @var int
 	 */
 	private $_id;
-	
+
 	/**
 	 * The description of the condition set
-	 * 
+	 *
 	 * @var string
 	 */
 	public $description;
-	
+
 	/**
 	 * The tree operation, represented by an operation object
-	 * 
+	 *
 	 * @var Operation
 	 */
 	private $_operation;
-	
+
 	/**
 	 * logical or arithmetical operation to be performed
 	 * between two groups of operands.
-	 * 
+	 *
 	 * For time being (02/dic/2013) a group is one single
 	 * column in the form used to define the condition set
-	 * 
+	 *
 	 * used also in the Operation::buildOperationTreeFromPOST method
-	 * 
+	 *
 	 * @var string
 	 */
 	public static $opBetweenGroups = ' || ';
-	
+
 	/**
 	 * logical or arithmetical operation to be performed
 	 * between two operators.
-	 * 
+	 *
 	 * For time being (02/dic/2013) an operator is one single
 	 * element selected in one dropdown list of the form
-	 * described above for the groups 
-	 * 
+	 * described above for the groups
+	 *
 	 * used also in the Operation::buildOperationTreeFromPOST method
-	 * 
+	 *
 	 * @var string
 	 */
 	public static $opBetweenOperands = ' && ';
-	
+
     /**
      * CompleteConditionSet constructor.
      */
     public function __construct($id=null, $description=null)
     {
     	$this->_operation = null;
-    	
+
     	if (!is_null($id)) $this->_id = $id;
-    	if (!is_null($description)) $this->description = $description;    	
+    	if (!is_null($description)) $this->description = $description;
     }
-    
+
     /**
      * completeConditionSet operation setter
-     * 
+     *
      * sets the operation property to the
      * passed Operation object
-     * 
+     *
      * @param Operation $op
      * @access public
      */
@@ -98,8 +98,8 @@ class CompleteConditionSet
     /**
      * gets all operands for a given priority.
      * if no priority is given, it gets all operands
-     * for all priorities in a 2-d array 
-     * 
+     * for all priorities in a 2-d array
+     *
      * @param  int $priority
      * @return array|null
      * @access public
@@ -108,12 +108,12 @@ class CompleteConditionSet
     {
     	$data = $this->toArray();
     	$retval = array();
-    	
+
     	if (!is_null($data))
     	{
 	    	foreach ($data as $op)
 	    	{
-	    		if (!is_array($retval[$op['priority']])) $retval[$op['priority']] = array();
+	    		if (!isset($retval[$op['priority']]) || (isset($retval[$op['priority']]) && !is_array($retval[$op['priority']]))) $retval[$op['priority']] = array();
 	    		if (!is_null($op['operand1']) && !strstr($op['operand1'], 'expr') && !in_array($op['operand1'], $retval[$op['priority']])) $retval[$op['priority']][] = $op['operand1'];
 	    		if (!is_null($op['operand2']) && !strstr($op['operand2'], 'expr') && !in_array($op['operand2'], $retval[$op['priority']])) $retval[$op['priority']][] = $op['operand2'];
 	    	}
@@ -124,7 +124,7 @@ class CompleteConditionSet
     /**
      * converts the operation object to an array
      *
-     * @return arrray|NULL
+     * @return array|NULL
      * @access public
      */
     public function toArray()
@@ -136,11 +136,11 @@ class CompleteConditionSet
     		return $test;
     	} else return null;
     }
-        
+
     /**
      * returns the string rapresentation of the
      * operation property
-     *      
+     *
      * @return null|string
      * @access public
      */
@@ -148,11 +148,11 @@ class CompleteConditionSet
     {
     	return !is_null($this->_operation) ? $this->_operation->toString() : null;
     }
-    
+
     /**
      * returns the evaluation of the operation property.
      * the returned type depends on the evaluated expression
-     * 
+     *
      * @param array $params the parameters to be passed for evaluation
      * @return null|mixed
      * @access public
@@ -161,10 +161,10 @@ class CompleteConditionSet
     {
     	return !is_null($this->_operation) ? $this->_operation->evaluate($params) : null;
     }
-    
+
     /**
      * id getter
-     * 
+     *
      * @return int|null
      * @access public
      */
@@ -172,10 +172,10 @@ class CompleteConditionSet
     {
     	return (intval($this->_id)>0) ? intval($this->_id) : null;
     }
-    
+
     /**
      * operation getter
-     * 
+     *
      * @return Operation
      * @access public
      */
