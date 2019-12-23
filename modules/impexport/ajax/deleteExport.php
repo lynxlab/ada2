c12cce690bde78ad697a4e35f0cbbe73d8a30c3a
Giorgio Consorti <g.consorti@lynxlab.com>
2019-12-23 16:06:22 +0100

    add repository listing feature with import and delete functionalities

---
+++ modules/impexport/ajax/deleteExport.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * IMPORT MODULE
+ *
+ * @package		export/import course
+ * @author		giorgio <g.consorti@lynxlab.com>
+ * @copyright	Copyright (c) 2019, Lynx s.r.l.
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html GNU Public License v.2
+ * @link		impexport
+ * @version		0.1
+ */
+
+/**
+ * Base config file
+ */
+require_once(realpath(dirname(__FILE__)) . '/../../../config_path.inc.php');
+
+/**
+ * Clear node and layout variable in $_SESSION
+*/
+$variableToClearAR = array('node', 'layout', 'course', 'user');
+
+/**
+ * Users (types) allowed to access this module.
+*/
+$allowedUsersAr = array(AMA_TYPE_SWITCHER, AMA_TYPE_AUTHOR);
+
+/**
+ * Get needed objects
+*/
+$neededObjAr = array(
+        AMA_TYPE_SWITCHER => array('layout'),
+        AMA_TYPE_AUTHOR => array('layout')
+);
+
+/**
+ * Performs basic controls before entering this module
+ */
+$trackPageToNavigationHistory = false;
+require_once ROOT_DIR.'/include/module_init.inc.php';
+require_once ROOT_DIR.'/browsing/include/browsing_functions.inc.php';
+BrowsingHelper::init($neededObjAr);
+
+// MODULE's OWN IMPORTS
+require_once MODULES_IMPEXPORT_PATH . '/config/config.inc.php';
+
+/**
+ * @var AMARepositoryDataHandler $rdh
+ */
+require_once MODULES_IMPEXPORT_PATH .'/include/AMARepositoryDataHandler.inc.php';
+$rdh = AMARepositoryDataHandler::instance();
+
+$retArray = array('status'=>'ERROR');
+
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
+	/**
+	 * it's a POST, delete the export with the passed id
+	 */
+	try {
+		$postParams = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+		$res = $rdh->deleteExport($postParams);
+		if (AMA_DB::isError($res)) {
+			// if it's an error display the error message
+			$retArray['status'] = "ERROR";
+			$retArray['msg'] = $res->getMessage();
+		} else {
+			$retArray['status'] = "OK";
+			$retArray['msg'] = translateFN('Esportazione cancellata');
+		}
+	} catch (\Exception $e) {
+		$retArray['status'] = "ERROR";
+		$retArray['msg'] = $e->getMessage();
+	}
+}
+
+header('Content-Type: application/json');
+echo json_encode($retArray);
---
+++ modules/impexport/js/repository.js
@@ -0,0 +1,109 @@
+/**
+ * IMPORT MODULE
+ *
+ * @package		export/import course
+ * @author		giorgio <g.consorti@lynxlab.com>
+ * @copyright	Copyright (c) 2019, Lynx s.r.l.
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html GNU Public License v.2
+ * @link		impexport
+ * @version		0.1
+ */
+
+function initDoc(canEdit) {
+    $j('#loader').fadeOut('fast', function(){
+        $j(this).remove();
+        $j('#pagecontainer > *, #footer').animate({ opacity:1 },'fast');
+	});
+    var dataTable = initDataTable(canEdit);
+
+	function initDataTable(canEdit) {
+		var data = {
+			what: 'Repository::List'
+		};
+
+		dataTable = $j('#repositoryList')
+		.DataTable({
+			"stateSave": true,
+			"stateDuration": -1,
+			"deferRender": true,
+	//		"serverSide" : true,
+			"processing" : true,
+			"autoWidth"  : false,
+			"rowId": 'id',
+			"searchDelay" : 500, //in millis
+			"order": [[ 3, 'desc' ], [ 1, 'asc' ]],
+			"retrieve": true,
+			"language": {
+				"url": HTTP_ROOT_DIR + "/js/include/jquery/dataTables/dataTablesLang.php"
+			},
+			"ajax" :  {
+                'type': 'GET',
+                'url' : "ajax/getTableData.php",
+                "data": function (d) {
+                    return $j.extend({}, d, data);
+                }
+			},
+			"columns": [
+                { "data": "title" },
+                { "data": "courseTitle", "visible": false },
+                { "data": "description" },
+                {
+                    "data": "exportDateTime",
+                    "className": "dt-body-center",
+                    "type": "date-euro",
+                    "width" : "8em"
+                },
+                { "data": "courseProvider" },
+                {
+                    "data": "actions",
+                    "orderable" : false,
+                    "searchable": false,
+                    "className": "dt-body-center actions",
+                    "width": "13em"
+                }
+            ],
+            "rowGroup": {
+                "dataSrc": 'courseTitle'
+            }
+		});
+		return dataTable;
+	} // initDataTable
+
+	this.deleteRepoItem = function(element, id) {
+		var theModal = $j('#deleteConfirm')
+		.modal('setting', {
+		  closable  : false,
+		  onDeny    : function(){
+			return true;
+		  },
+		  onApprove : function() {
+            //   alert('Implement');
+            //   theModal.modal('hide');
+			$j.ajax({
+				type: 'POST',
+				url: 'ajax/deleteExport.php',
+				data: { id: id },
+				dataType: 'json',
+				beforeSend: function() {
+					theModal.modal('hide');
+				}
+			})
+			.done(function(JSONObj) {
+				if (JSONObj.status.length > 0) {
+					$j.when(showHideDiv('', JSONObj.msg, JSONObj.status == 'OK')).then(function () {
+						if (JSONObj.status == 'OK') dataTable.ajax.reload(null, false);
+					});
+				}
+			})
+			.fail(function() {
+				showHideDiv('', $j('#unknownErrorMSG').html(), false);
+			});
+			return false;
+		  }
+		})
+		.modal('show');
+	}
+
+} // initDoc
+
+
