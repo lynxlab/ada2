7efaa6bc7d69e541cb15ae3926a97267e2d2469f
Giorgio Consorti <g.consorti@lynxlab.com>
2019-12-23 16:04:18 +0100

    add exporter_userid field sving and getRepositoryList, deleteExport, getTesterIDFromPointer methods

--- modules/impexport/include/AMARepositoryDataHandler.inc.php
+++ modules/impexport/include/AMARepositoryDataHandler.inc.php
@@ -25,6 +25,7 @@ class AMARepositoryDataHandler extends \AMA_Common_DataHandler {
 	public function saveExportData(array $saveData=[]) {
 		$isUpdate = array_key_exists('id', $saveData) && intval($saveData['id'])>0;
 		if (!array_key_exists('exportTS', $saveData)) $saveData['exportTS'] = $this->date_to_ts('now');
+		if (!array_key_exists('exporter_userid', $saveData)) $saveData['exporter_userid'] = $_SESSION['sess_userObj']->getId();
 		if (!$isUpdate) {
 			$result = $this->executeCriticalPrepared(
 				$this->sqlInsert(
@@ -50,6 +51,93 @@ class AMARepositoryDataHandler extends \AMA_Common_DataHandler {
 		if (!\AMA_DB::isError($result)) {
 			return $saveData;
 		} else throw new Exception($result->getMessage(), $result->getCode());
+	}
+
+	/**
+	 * Gets repository items
+	 *
+	 * @param array $whereArr
+	 * @return array
+	 */
+	public function getRepositoryList (array $whereArr=[]) {
+		$sql = 'SELECT R.*,CONCAT(U.nome," ",U.cognome) AS exporter_fullname FROM `'.self::PREFIX.'repository` R LEFT JOIN `utente` U ON U.`id_utente` = R.`exporter_userid`'
+				.$this->buildWhereClause($whereArr, array_keys($whereArr));
+		$res = $this->getAllPrepared($sql, array_values($whereArr), AMA_FETCH_ASSOC);
+
+		if (!\AMA_DB::isError($res)) {
+			/**
+			 * this is needed to instantiate the proper dh and get course data
+			 */
+			$testersArr = $this->get_all_testers(['id_tester']);
+			$cachedValues = ['courseTitles' => [], 'courseProviders' => []];
+			$res = array_map(function($element) use ($testersArr, &$cachedValues) {
+				if (!array_key_exists($element['id_course'], $cachedValues['courseTitles'])) {
+					$provider = array_filter($testersArr, function ($el) use ($element) {
+						// var_dump([$el, $element]);
+						return $el['id_tester'] == $element['id_tester'];
+					});
+					$provider = reset($provider);
+					$pdh = \AMA_DataHandler::instance(\MultiPort::getDSN($provider['puntatore']));
+					$courseData = $pdh->get_course($element['id_course']);
+					if (!\AMA_DB::isError($courseData)) {
+						$cachedValues['courseTitles'][$element['id_course']] = $courseData['titolo'];
+					}
+					$cachedValues['courseProviders'][$element['id_course']] = $provider['puntatore'];
+				}
+				$element['courseProvider'] = $cachedValues['courseProviders'][$element['id_course']];
+				$element['courseTitle'] = $cachedValues['courseTitles'][$element['id_course']];
+				$element['exportDateTime'] = ts2dFN($element['exportTS']).' '.ts2tmFN($element['exportTS']);
+
+				return $element;
+			}, $res);
+			return $res;
+		} else return [];
+	}
+
+	/**
+	 * Deletes export data to the repository table
+	 *
+	 * @param array $delData
+	 * @return boolean
+	 * @throws Exception
+	 */
+	public function deleteExport(array $delData=[]) {
+		if (array_key_exists('id', $delData) && intval($delData['id'])>0) {
+			$toDelete = $this->getRepositoryList(['id' => $delData['id']]);
+			if (is_array($toDelete) && count($toDelete)==1) {
+				$toDelete = reset($toDelete);
+				$delArr = ['id' => $toDelete['id'] ];
+				$result = $this->queryPrepared(
+					$this->sqlDelete(
+						self::PREFIX.'repository',
+						$delArr
+					),
+					array_values($delArr)
+				);
+				if (!\AMA_DB::isError($result)) {
+					$repodir = MODULES_IMPEXPORT_REPOBASEDIR .$toDelete['id_course'] . DIRECTORY_SEPARATOR . MODULES_IMPEXPORT_REPODIR;
+					@unlink($repodir. DIRECTORY_SEPARATOR. $toDelete['filename']);
+					@rmdir ($repodir);
+					return true;
+				} else throw new Exception($result->getMessage());
+			} else throw new Exception(translateFN('Errore nella lettura dati'));
+		} else throw new Exception(translateFN('ID export non valido'));
+	}
+
+	/**
+	 * Gets a tester id from its pointer
+	 *
+	 * @param string $pointer
+	 * @return int
+	 */
+	public function getTesterIDFromPointer($pointer = null) {
+		$testerId = null;
+		if (is_null($pointer)) $pointer = $_SESSION['sess_selected_tester'];
+		$testerInfo = $this->get_tester_info_from_pointer($pointer);
+		if (!AMA_DB::isError($testerInfo) && is_array($testerInfo) && isset($testerInfo[0])) {
+			$testerId = $testerInfo[0];
+		}
+		return $testerId;
 	}
 
 	/**
