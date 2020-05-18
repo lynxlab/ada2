6f4f495b0087188c5bcf3d79d56ca8cd76838449
Giorgio Consorti <g.consorti@lynxlab.com>
2020-05-18 12:45:08 +0200

    add basic support for Jitsi videochat

--- comunica/include/videochat_config_DEFAULT.inc.php
+++ comunica/include/videochat_config_DEFAULT.inc.php
@@ -1,7 +1,8 @@
 <?php
+
 /**
  * Conference generic configuration file.
- * 
+ *
  * @author
  * @version
  * @package
@@ -9,14 +10,25 @@
  * @copyright (c) 2009 Lynx s.r.l.
  */
 
-/** 
+/**
+ * if it's not a multiprovider environment
+ * include provider videochat_config file
+ */
+if (!MULTIPROVIDER && isset($GLOBALS['user_provider']) && !empty($GLOBALS['user_provider']) && is_readable(ROOT_DIR . '/clients/' . $GLOBALS['user_provider'] . '/videochat_config.inc.php')) {
+    require_once ROOT_DIR . '/clients/' . $GLOBALS['user_provider'] . '/videochat_config.inc.php';
+}
+/**
  * Type of conference integrated
-*/
-define ('CONFERENCE_TO_INCLUDE','OpenMeeting'); // OpenMeeting
-//define ('CONFERENCE_TO_INCLUDE','AdobeConnect'); //Adobe Connect
+ */
+if (!defined('CONFERENCE_TO_INCLUDE')) {
+    define('CONFERENCE_TO_INCLUDE', 'OpenMeeting'); // OpenMeeting
+    // define ('CONFERENCE_TO_INCLUDE','Jitsi'); // Jitsi
+    //define ('CONFERENCE_TO_INCLUDE','AdobeConnect'); //Adobe Connect
+}
 
-/** 
+/**
  * Has to control if appoint is defined
-*/
-define('DATE_CONTROL',FALSE);
-
+ */
+if (!defined('DATE_CONTROL')) {
+    define('DATE_CONTROL', FALSE);
+}
