30821507d3a6f8407f948d8317b1828579d70747
Giorgio Consorti <g.consorti@lynxlab.com>
2020-08-13 12:40:05 +0200

    add BigBlueButton config and base class

---
+++ comunica/include/BigBlueButton.config_DEFAULT.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * BigBlueButton Conference specific configuration file.
+ *
+ * @author Giorgio
+ * @version 0.1
+ * @package videochat
+ * @license GPL 2.0
+ * @copyright (c) 2020 Lynx s.r.l.
+ */
+
+/**
+ *
+ * @name CONNECT DATA SERVER
+ */
+define('BBB_SERVER', 'https://yourserver.com');
+define('BBB_SERVER_BASE_URL' , BBB_SERVER . '/bigbluebutton/');
+define('BBB_SECRET', '');
+
+define('FRAME_WIDTH','90%');
+define('FRAME_HEIGHT','700px');
