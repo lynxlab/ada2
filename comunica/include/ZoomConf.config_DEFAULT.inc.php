e63b9491e71bcbce5ecb65bd00bb31fa2e9fd8a6
Giorgio Consorti <g.consorti@lynxlab.com>
2020-09-18 09:53:32 +0200

    add zoom conference class and configuration

---
+++ comunica/include/ZoomConf.config_DEFAULT.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Zoom Conference specific configuration file.
+ *
+ * @author Giorgio
+ * @version 0.1
+ * @package videochat
+ * @license GPL 2.0
+ * @copyright (c) 2020 Lynx s.r.l.
+ */
+
+/**
+ *
+ * @name CONNECT DATA SERVER
+ */
+define('ZOOMCONF_APIKEY', '<YOUR APIKEY HERE>');
+define('ZOOMCONF_APISECRET', '<YOUR API SECRET>');
+
+define('FRAME_WIDTH','90%');
+define('FRAME_HEIGHT','700px');
