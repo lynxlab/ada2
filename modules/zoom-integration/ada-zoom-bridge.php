7baebf0f4c8e46d0fa8cd48ef5a37d9211ca3356
Giorgio Consorti <g.consorti@lynxlab.com>
2021-01-19 15:01:14 +0100

    update Zoom Web SDK to v1.8.6

--- modules/zoom-integration/ada-zoom-bridge.php
+++ modules/zoom-integration/ada-zoom-bridge.php
@@ -27,8 +27,8 @@ if (!isset($_SESSION['ada-zoom-bridge'])) {
     <meta charset="utf-8" />
 
     <!-- import #zmmtg-root css -->
-    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/1.8.0/css/bootstrap.css" />
-    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/1.8.0/css/react-select.css" />
+    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/1.8.6/css/bootstrap.css" />
+    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/1.8.6/css/react-select.css" />
 
     <meta name="format-detection" content="telephone=no">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
@@ -38,15 +38,14 @@ if (!isset($_SESSION['ada-zoom-bridge'])) {
     <main></main>
     <footer></footer>
     <!-- import ZoomMtg dependencies -->
-    <script src="https://source.zoom.us/1.8.0/lib/vendor/react.min.js"></script>
-    <script src="https://source.zoom.us/1.8.0/lib/vendor/react-dom.min.js"></script>
-    <script src="https://source.zoom.us/1.8.0/lib/vendor/redux.min.js"></script>
-    <script src="https://source.zoom.us/1.8.0/lib/vendor/redux-thunk.min.js"></script>
-    <script src="https://source.zoom.us/1.8.0/lib/vendor/jquery.min.js"></script>
-    <script src="https://source.zoom.us/1.8.0/lib/vendor/lodash.min.js"></script>
+    <script src="https://source.zoom.us/1.8.6/lib/vendor/react.min.js"></script>
+    <script src="https://source.zoom.us/1.8.6/lib/vendor/react-dom.min.js"></script>
+    <script src="https://source.zoom.us/1.8.6/lib/vendor/redux.min.js"></script>
+    <script src="https://source.zoom.us/1.8.6/lib/vendor/redux-thunk.min.js"></script>
+    <script src="https://source.zoom.us/1.8.6/lib/vendor/lodash.min.js"></script>
 
     <!-- import ZoomMtg -->
-    <script src="https://source.zoom.us/zoom-meeting-1.8.0.min.js"></script>
+    <script src="https://source.zoom.us/zoom-meeting-1.8.6.min.js"></script>
 
     <!-- import local .js file -->
     <script src="ada-zoom.js.php"></script>
