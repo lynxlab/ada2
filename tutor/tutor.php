ff8bc331ce06fa333eb2f780561cf2016af5d767
Giorgio Consorti <g.consorti@lynxlab.com>
2021-02-18 18:09:17 +0100

    add include to use the complete condition summary modal

--- tutor/tutor.php
+++ tutor/tutor.php
@@ -568,4 +568,10 @@ if (isset($id_course) && intval($id_course)>0 && isset($id_instance) && intval($
 	$optionsAr['onload_func'] .= $id_course.','.$id_instance;
 $optionsAr['onload_func'] .= ');';
 
+if (defined('MODULES_SERVICECOMPLETE') && MODULES_SERVICECOMPLETE && in_array($op, ['student', 'class_report'])) {
+    $layout_dataAr['JS_filename'][] = MODULES_SERVICECOMPLETE_PATH . '/js/condition-recap-modal.js';
+    $layout_dataAr['CSS_filename'][] = MODULES_SERVICECOMPLETE_PATH . '/layout/'.$template_family.'/css/condition-recap.css';
+    $optionsAr['onload_func'] .= 'initSummaryModal(\''.str_replace(ROOT_DIR, '', MODULES_SERVICECOMPLETE_PATH).'\')';
+}
+
 ARE::render($layout_dataAr, $content_dataAr,NULL,$optionsAr,$menuOptions);
