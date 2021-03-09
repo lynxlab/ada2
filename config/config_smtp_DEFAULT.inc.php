c1aa91373d6de4d8991b97ef423229f5810a3a79
Giorgio Consorti <g.consorti@lynxlab.com>
2021-03-09 10:54:21 +0100

    add smtp auth type support

--- config/config_smtp_DEFAULT.inc.php
+++ config/config_smtp_DEFAULT.inc.php
@@ -5,6 +5,7 @@
 define ('ADA_SMTP_HOST', 'localhost');
 define ('ADA_SMTP_PORT', 25);
 define ('ADA_SMTP_AUTH', false);
+define ('ADA_SMTP_AUTHTYPE', ''); // One of: 'CRAM-MD5', 'LOGIN', 'PLAIN', 'XOAUTH2' or leave empty
 define ('ADA_SMTP_SECURE', null); // use PHPMailer constants (like PHPMailer::ENCRYPTION_SMTPS) or null
 define ('ADA_SMTP_USERNAME', '');
 define ('ADA_SMTP_PASSWORD', '');
